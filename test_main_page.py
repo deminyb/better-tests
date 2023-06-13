c09b5ea54060802df20add6ae18af08517f2ef28
Iurii Demin <YDemin@it-one.ru>
2023-06-13 17:00:11 +0300

    new class and login page locators were added

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,15 @@
-from selenium import webdriver
+from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/"
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()                           # открываем страницу в браузере
+    page.go_to_login_page()             # выполняем метод страницы — переходим на страницу логина
+
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
