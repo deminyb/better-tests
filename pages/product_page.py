976782c06ae354d12139335b7579c9980818d0d7
Iurii Demin <YDemin@it-one.ru>
2023-06-16 13:28:27 +0300

    fixed issues with dots in deps

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,5 @@
-from pages.base_page import BasePage
-from pages.locators import ProductPageLocators, BasePageLocators
+from .base_page import BasePage
+from .locators import ProductPageLocators, BasePageLocators
 
 
 class ProductPage(BasePage):
